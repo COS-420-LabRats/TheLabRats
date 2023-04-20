529f6bd2dd1a6fe4150bea51dd6f7c69de906d5e
Kenzie Young <mackenzie.k.young@maine.edu>
2023-04-20 19:02:16 -0400

    Home fragment fix- toolbar still not showing up

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -7,8 +7,6 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 
@@ -16,20 +14,10 @@ class HomeFragment : Fragment() {
     private lateinit var logout: Button
     private lateinit var firebaseAuth: FirebaseAuth
 
-   /* override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-        logout = view.findViewById(R.id.logoutButton)
-        logout.setOnClickListener { logOut() }
-
-        val toolbar = view.findViewById<Toolbar>(R.id.HomeToolbar)
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
-
-        firebaseAuth = FirebaseAuth.getInstance()
-
-        setupMenu() // enable options menu
-
-        return view
-    }*/
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -37,38 +25,35 @@ class HomeFragment : Fragment() {
         logout = view.findViewById(R.id.logoutButton)
         logout.setOnClickListener { logOut() }
 
-        val toolbar = view.findViewById<Toolbar>(R.id.HomeToolbar)
+        // Create a new instance of Toolbar
+        val toolbar = Toolbar(requireContext())
+        // Set the toolbar as the support action bar for the activity
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        // Set the title for the toolbar
+        toolbar.title = "My Toolbar Title"
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
-
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.top_menu, menu)
-            }
-
-            override fun onMenuItemSelected(item: MenuItem): Boolean {
-                return when (item.itemId) {
-                    R.id.Settings -> {
-                        Toast.makeText(context, "You clicked Settings", Toast.LENGTH_LONG).show()
-                        true
-                    }
-                    R.id.UpdateProfile -> {
-                        Toast.makeText(context, "You clicked Update Profile", Toast.LENGTH_LONG)
-                            .show()
-                        true
-                    }
-                    R.id.UpdatePreferences -> {
-                        Toast.makeText(context, "You clicked Update Preferences", Toast.LENGTH_LONG)
-                            .show()
-                        true
-                    }
-                    else -> false
+        // Set up the options menu for the toolbar
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.Settings -> {
+                    Toast.makeText(context, "You clicked Settings", Toast.LENGTH_LONG).show()
+                    true
                 }
+                R.id.UpdateProfile -> {
+                    Toast.makeText(context, "You clicked Update Profile", Toast.LENGTH_LONG)
+                        .show()
+                    true
+                }
+                R.id.UpdatePreferences -> {
+                    Toast.makeText(context, "You clicked Update Preferences", Toast.LENGTH_LONG)
+                        .show()
+                    true
+                }
+                else -> false
             }
-
-        }, viewLifecycleOwner)
+        }
     }
 
     private fun logOut() {
